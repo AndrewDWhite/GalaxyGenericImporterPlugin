cfd32a2e7663f72a1cc966579143f177f28ea694
Andrew David White <972757+AndrewDWhite@users.noreply.github.com>
2020-06-18 21:34:40 -0400

    Updated with additional code to ignore

--- coverage.sh
+++ coverage.sh
@@ -1,3 +1,3 @@
 py  -3.7-32 -m pip install coverage
 py  -3.7-32 -m coverage run UnittestProject.py
-py  -3.7-32 -m coverage html --omit=escapejson/*,galaxy/*,galaxyutils/*,parameterized/*
+py  -3.7-32 -m coverage html --omit=escapejson/*,galaxy/*,galaxyutils/*,parameterized/*,win32/*
