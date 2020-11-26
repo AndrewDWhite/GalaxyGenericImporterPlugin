0cda8a63433938c7de13412ec525af8639935c2f
Andrew David White <>
2020-11-26 00:55:24 -0500

    Updated to do come cleanup after coverage

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,5 @@
 py  -3.7-32 -m pip install coverage
 py  -3.7-32 -m coverage run UnittestProject.py
 py  -3.7-32 -m coverage html --omit=escapejson/*,galaxy/*,galaxyutils/*,parameterized/*,win32/*,wrapt/*,aiounittest/*,syncasync.py
+
+for /f %%i in ('dir /b TestDirectory*') do rd /s /q %%i
