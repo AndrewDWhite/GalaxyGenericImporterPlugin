4d4a75cd5a7b08938e02ae527c0239978e7d8373
Andrew David White <unknown>
2021-06-01 21:36:42 -0400

    Updated unit tests to hopefuly fail build if they fail

--- coverage.bat
+++ coverage.bat
@@ -1,5 +1,14 @@
-py  -3.7-32 -m pip install coverage
-py  -3.7-32 -m coverage run UnittestProject.py
+set my_error=0
+
+py  -3.7-32 -m pip install coverage pytest pytest-cov
+:: py  -3.7-32 -m coverage run UnittestProject.py
+py  -3.7-32 -m pytest UnittestProject.py --cov --junit-xml pytest.xml
+IF %ERRORLEVEL% NEQ 0 ( 
+   set my_error=%ERRORLEVEL%
+)
 py  -3.7-32 -m coverage html --omit=escapejson/*,galaxy/*,galaxyutils/*,parameterized/*,win32/*,wrapt/*,aiounittest/*,syncasync.py
 
+
 for /f %%i in ('dir /b TestDirectory*') do rd /s /q %%i
+
+EXIT /B %my_error% 
