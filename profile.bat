d4c59cd0ad59cded9600e26bed2f3ac903e7554a
Andrew David White <>
2021-08-17 22:00:47 -0400

    Updated to fix it to allow github to determine that an error occured and moved profileing to new file

--- profile.bat
+++ profile.bat
@@ -1,13 +1,5 @@
-set my_error=0
-
 py  -3.7-32 -m pip install -r ./requirements_dev.txt --target ./src/
-py  -3.7-32 -m pytest src/UnittestProject.py --cov --junit-xml pytest.xml
-IF %ERRORLEVEL% NEQ 0 ( 
-   set my_error=%ERRORLEVEL%
-)
+py  -3.7-32 -m cProfile -s cumulative -m pytest src/UnittestProject.py --cov --junit-xml pytest.xml
 py  -3.7-32 -m coverage html --omit=escapejson/*,galaxy/*,galaxyutils/*,parameterized/*,win32/*,wrapt/*,aiounittest/*,syncasync.py
 
-
 for /f %%i in ('dir /b TestDirectory*') do rd /s /q %%i
-
-EXIT /B %my_error% 
